54fef26aa3d214bbeda47248b79157dbff582c19
fzubero <fernando.zubero@empliq.com>
2025-03-06 15:04:22 +0100

    re

--- index.js
+++ index.js
@@ -284,6 +284,7 @@ if (!nativeBinding) {
   throw new Error(`Failed to load native binding`)
 }
 
-const { plus100 } = nativeBinding
+const { plus100, ping } = nativeBinding
 
 module.exports.plus100 = plus100
+module.exports.ping = ping
