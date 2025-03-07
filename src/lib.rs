45f8c9b6da9891473733ad137b4ea95ab04ff295
Jan Mechtel <jmechtel@gmail.com>
2025-03-07 12:25:29 +0100

    changed to mylib to k21, removed sample plus100 method

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,7 @@
 use napi_derive::napi;
 
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100
-}
-
-#[napi]
 pub fn ping() -> String {
   println!("ping");
-  mylib::signal::utils::ping()
+  k21::signal::utils::ping()
 }
